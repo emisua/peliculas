ad9a7aaa62dc46c9383193cf9ad572745c280299
Emilio Suárez <91371538+emisua@users.noreply.github.com>
2023-04-24 23:52:55 +0200

    hook para obtener las peliculas

--- src/hooks/useMovies.jsx
+++ src/hooks/useMovies.jsx
@@ -1,14 +1,28 @@
-import responseMovies from '../mock/movies.json'
+import { useState, useRef } from 'react'
+import { searchMovies } from '../services/movies'
 
-export function useMovies() {
-	const movies = responseMovies?.Search
-	const mappedMovies = movies.map((movie) => {
-		return {
-			id: movie.imdbID,
-			title: movie.Title,
-			poster: movie.Poster,
-			year: movie.Year
+export function useMovies({ search }) {
+	const [movies, setMovies] = useState([])
+	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState(null)
+	// Guardo la busqueda anterior
+	const previousSearch = useRef(search)
+
+	const getMovies = async () => {
+		// Si la busqueda anterior es igual a la nueva busqueda, no hago la llamada
+		if (previousSearch.current === search) return
+		try {
+			setLoading(true)
+			setError(null)
+			// Seteo en el ref la nueva busqueda
+			previousSearch.current = search
+			const newMovies = await searchMovies({ search })
+			setMovies(newMovies)
+		} catch (err) {
+			setError(err.message)
+		} finally {
+			setLoading(false)
 		}
-	})
-	return { mappedMovies }
+	}
+	return { movies, getMovies, error, loading }
 }
