4aa9175ac0e6aeaa2ca7e994cab489d3c32f64d9
Emilio Suárez <91371538+emisua@users.noreply.github.com>
2023-04-24 23:52:44 +0200

    hook para buscar peliculas

--- src/hooks/useSearch.jsx
+++ src/hooks/useSearch.jsx
@@ -1,10 +1,16 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 function useSearch() {
 	const [search, setSearch] = useState('')
 	const [error, setError] = useState('')
+	const isFirstInput = useRef(true)
 
 	useEffect(() => {
+		if (isFirstInput.current) {
+			// Si al entrar a la app, el input es = a '', es la primera vez que entra a la app. Por lo tanto, no valida nada y espera a que se haya escrito al menos una vez en ese input.
+			isFirstInput.current = search === ''
+			return
+		}
 		if (search === '') {
 			setError('Escribe el nombre de la película para buscar')
 			return
@@ -16,7 +22,7 @@ function useSearch() {
 		setError(null)
 	}, [search])
 
-	return { search, setSearch, error }
+	return { search, setSearch, error, isFirstInput }
 }
 
 export default useSearch
